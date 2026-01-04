23dbf8d495b490d8092d5202977f22e5780a301c
Muritala David Ilerioluwa <ileri.engineering@gmail.com>
2026-01-04 13:57:25 +0100

    Update config

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/dotfiles"
+
+
+ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES_DIR/ghostty-config" "$HOME/.config/ghostty/config"
+ln -sf "$DOTFILES_DIR/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/gitconfig" "$HOME/.gitconfig"
