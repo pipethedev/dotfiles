dd68c1f3b7b6a7c73319352149b87ce7e6a3cfda
Muritala David Ilerioluwa <ileri.engineering@gmail.com>
2026-01-04 14:41:05 +0100

    updated formatters

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,21 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    go = { "gofumpt", "goimports" },
+    php = { "php_cs_fixer" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
